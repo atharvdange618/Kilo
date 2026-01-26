9eb92b7e1bf762ac93d99c647d8cc1c0de5b527d
Atharv Dange <atharvdange618@gmail.com>
2026-01-26 17:31:57 +0530

    feat: implement vertical scrolling

--- kilo.c
+++ kilo.c
@@ -73,6 +73,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int rowoff;
   int screenrows;
   int screencols;
   int numrows;
@@ -507,6 +508,16 @@ void abFree(struct abuf *ab) { free(ab->b); }
  * - Screen refresh and clearing
  */
 
+void editorScroll() {
+  if(E.cy < E.rowoff) {
+    E.rowoff = E.cy;
+  }
+  if(E.cy >= E.rowoff + E.screenrows) {
+    E.rowoff = E.cy - E.screenrows + 1;
+  }
+}
+
+
 /*
  * editorDrawRows() - Draw tilde rows on screen
  *
@@ -517,7 +528,8 @@ void editorDrawRows(struct abuf *ab) {
   int y;
 
   for (y = 0; y < E.screenrows; y++) {
-    if(y >= E.numrows) {
+    int filerow = y + E.rowoff;
+    if(filerow >= E.numrows) {
       if (E.numrows == 0 && y == E.screenrows / 3) {
         char welcome[80];
         int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -536,9 +548,9 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[y].size;
+      int len = E.row[filerow].size;
       if(len > E.screencols) len = E.screencols;
-      abAppend(ab, E.row[y].chars, len);
+      abAppend(ab, E.row[filerow].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -574,6 +586,8 @@ void editorDrawRows(struct abuf *ab) {
  * We use VT100 sequences for broad terminal emulator compatibility.
  */
 void editorRefreshScreen() {
+  editorScroll();
+
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
@@ -582,7 +596,7 @@ void editorRefreshScreen() {
   editorDrawRows(&ab);
 
   char buf[32];
-  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff), E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -639,7 +653,7 @@ void editorMoveCursor(int key) {
     }
     break;
   case ARROW_DOWN:
-    if (E.cy != E.screenrows - 1) {
+    if (E.cy < E.numrows) {
       E.cy++;
     }
     break;
@@ -704,6 +718,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rowoff = 0;
   E.numrows = 0;
   E.row = NULL;
 
